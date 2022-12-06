3570a81ff3764ad05cc48b1b87bed0ab7e228a94
RumyantsevAlexandr <116197721+AleksandrRumyantsev@users.noreply.github.com>
2022-12-06 20:19:59 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Проверяем работу
+        Phone p = new Phone("123", "1234567");
+        Phone p2 = new Phone("1234", "123456");
+        //Phone p3 = new Phone("123", "123456");.............Ошибка
+        //Phone p4 = new Phone("1234", "1234567");.............Ошибка
+        //Phone p5 = new Phone("1234", "1234567");.............Ошибка
+        //Phone p6 = new Phone("12345", "1234567");.............Ошибка
+        //Phone p6 = new Phone("12345", "12345678");............Ошибка
+        System.out.println(p.toString());
+        System.out.println(p2.toString());
+        System.out.println();
+
+        //Пример с множествами:
+        System.out.println("Проверяем работу с множествами:");
+        HashSet <Phone> hashSet = new HashSet<>();
+        hashSet.add(new Phone("123","1234567"));
+        hashSet.add(new Phone("1234","123456"));
+        hashSet.add(new Phone("123","1234567"));
+        hashSet.add(new Phone("1234","123456"));
+        hashSet.add(new Phone("124","1234568"));
+        hashSet.add(new Phone("1235","123457"));
+        hashSet.add(new Phone("123","1234568"));
+        hashSet.add(new Phone("1235","123456"));
+        //hashSet.add(new Phone("1234","1234567")); .............Ошибка
+        //hashSet.add(new Phone("123","123456"));.............Ошибка
+        Iterator<Phone> iterator = hashSet.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next().toString());
+        }
+        System.out.println();
+
+        //Пример с ассоциативными массивами:
+        System.out.println("Проверяем работы с ассоциативными массивами:");
+        HashMap hashMap = new HashMap();
+        hashMap.put(new Phone("123","1234567"), "Иванов");
+        hashMap.put(new Phone("1234","123456"), "Петров");
+        hashMap.put(new Phone("123","1234567"), "Сидоров");
+        hashMap.put(new Phone("1234","123456"), "Таибов");
+        hashMap.put(new Phone("124","1234568"), "Румянцев");
+        hashMap.put(new Phone("1235","123457"), "Козлов");
+        hashMap.put(new Phone("123","1234568"), "Путин");
+        hashMap.put(new Phone("1235","123456"), "Навальный");
+        System.out.println(hashMap);
+    }
+}
---
+++ Phone.java
@@ -0,0 +1,45 @@
+import java.util.Objects;
+
+public class Phone {
+    private String codeRegion;
+    private String phoneNumber;
+
+    public Phone(String codeRegion, String phoneNumber) {
+        if ((codeRegion.matches("\\d{3}")&&(phoneNumber.matches("\\d{7}")))||(codeRegion.matches("\\d{4}")&&(phoneNumber.matches("\\d{6}"))))
+        {
+            this.codeRegion = codeRegion;
+            this.phoneNumber = phoneNumber;
+        }else{
+            throw new IllegalArgumentException("Ведено некорректное значение, введите значение формата (ХХХ) ХХХ-ХХ-ХХ или (ХХХХ) ХХ-ХХ-ХХ.");
+        }
+    }
+
+    public String getCodeRegion() {
+        return codeRegion;
+    }
+
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+            Phone phone = (Phone) o;
+        return Objects.equals(codeRegion, phone.codeRegion) && Objects.equals(phoneNumber, phone.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codeRegion, phoneNumber);
+    }
+
+    @Override
+    public String toString() {
+//        Pattern pattern = Pattern.compile("\\d{2,3}[\\s-]?\\d{2}[\\s-]?\\d{2}");
+        return "("+getCodeRegion()+") "+ getPhoneNumber().replaceFirst("(\\d{2,3})(\\d{2})(\\d{2})", "$1-$2-$3");
+    }
+
+}
